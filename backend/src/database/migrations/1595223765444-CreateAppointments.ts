14a461767a5bf877627a8fd7b1541351f281b098
RBioZ <jonatha_rihan@hotmail.com>
2020-07-27 01:11:16 -0300

    Aula 17 - Repositório Typeorm

--- backend/src/database/migrations/1595223765444-CreateAppointments.ts
+++ backend/src/database/migrations/1595223765444-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1595223765444
 						type: 'varchar',
 						isPrimary: true,
 						generationStrategy: 'uuid',
+						default: 'uuid_generate_v4()',
 					},
 					{
 						name: 'provider',
